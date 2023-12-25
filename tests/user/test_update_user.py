f88a1b66be507fa3d178474c900577686e33eaea
Y3ll0w <k.varbarkin@gmail.com>
2023-12-25 21:43:35 +0300

    Fix test tess_update_user

--- tests/user/test_update_user.py
+++ tests/user/test_update_user.py
@@ -1,11 +1,8 @@
 from utils.User import UserApi
-import pytest
 
 
 class TestCreateUser:
-    @pytest.mark.xfail
     def test_user_update(self):
         """Создаем пользователя"""
         user_data = UserApi.create()
-        updated_user_data = UserApi.user_update(username=user_data['username'])
-        UserApi.get_user_by_username(username=updated_user_data['username'])
+        UserApi.user_update(username=user_data['username'])
