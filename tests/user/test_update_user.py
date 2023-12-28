89cbd642d60a6ad953c3c94469d8d841412d229d
Y3ll0w <k.varbarkin@gmail.com>
2023-12-28 19:34:05 +0300

    Added mark in user api tests

--- tests/user/test_update_user.py
+++ tests/user/test_update_user.py
@@ -1,8 +1,18 @@
+import allure
+
 from petstore_api_test_framework.utils.user_api.create import create
 from petstore_api_test_framework.utils.user_api.update import update
 from petstore_api_test_framework.utils.user_api.get_user_by_username import get_user_by_username
 
 
+@allure.epic('User API')
+@allure.story('Update user')
+@allure.title('Update user')
+@allure.feature('Update user API')
+@allure.label('microservice', 'API')
+@allure.label('owner', 'allure8')
+@allure.tag('regress', 'API', 'normal')
+@allure.severity('normal')
 def test_update_user(api_url, headers):
     # WHEN
     user_data = create(api_url, headers)
--- tests/user/test_user_logout.py
+++
@@ -1,6 +0,0 @@
-from petstore_api_test_framework.utils.user_api.logout import logout
-
-
-def test_user_logout(api_url, headers):
-    # THEN
-    logout(api_url, headers)
